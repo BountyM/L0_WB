48f6581594d36dd31a90b9061f3dfafc89abdd85
Maxim Kuzin <BountyMaxim@yandex.ru>
2023-10-31 12:59:26 +0300

    need db and nats

--- models/order.go
+++ models/order.go
@@ -7,12 +7,12 @@ type Order struct {
 	Delivery           Delivery `json:"delivery"`
 	Payment            Payment  `json:"payment"`
 	Items              []Item   `json:"items"`
-	Locale             string   `json:"locale" db:"chrt_id"`
-	Internal_signature string   `json:"internal_signature" db:"chrt_id"`
-	Customer_id        string   `json:"customer_id" db:"chrt_id"`
-	Delivery_service   string   `json:"delivery_service" db:"chrt_id"`
-	Shardkey           string   `json:"shardkey" db:"chrt_id"`
-	Sm_id              int      `json:"sm_id" db:"chrt_id"`
-	Date_created       string   `json:"date_created" db:"chrt_id"`
-	Oof_shard          string   `json:"oof_shard" db:"chrt_id"`
+	Locale             string   `json:"locale" db:"locale"`
+	Internal_signature string   `json:"internal_signature" db:"internal_signature"`
+	Customer_id        string   `json:"customer_id" db:"customer_id"`
+	Delivery_service   string   `json:"delivery_service" db:"delivery_service"`
+	Shardkey           string   `json:"shardkey" db:"shardkey"`
+	Sm_id              int      `json:"sm_id" db:"sm_id"`
+	Date_created       string   `json:"date_created" db:"date_created"`
+	Oof_shard          string   `json:"oof_shard" db:"oof_shard"`
 }
