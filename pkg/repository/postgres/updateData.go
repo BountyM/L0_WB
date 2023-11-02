67e309af0db07f0570469c0c743118f1e4973c81
Maxim Kuzin <BountyMaxim@yandex.ru>
2023-11-02 14:59:24 +0300

    add update db

--- pkg/repository/postgres/updateData.go
+++ pkg/repository/postgres/updateData.go
@@ -22,7 +22,7 @@ func UpdateOrder(order models.Order, db *sqlx.DB) {
 	}
 
 	if _, err := GetPayment(db, order.Order_uid); err != nil {
-		UpdatePayment(order.Delivery, db, order.Order_uid)
+		UpdatePayment(order.Payment, db)
 	} else {
 		SetPayment(order.Payment, db)
 	}
@@ -43,11 +43,24 @@ func UpdateDelivery(delivery models.Delivery, db *sqlx.DB, uid string) {
 	}
 }
 
-func UpdatePayment(delivery models.Delivery, db *sqlx.DB, uid string) {
-	query := `UPDATE deliveries SET name=$2, phone=$3, zip=$4, city=$5, address=$6, 
-	region=$7, email=$8 WHERE order_uid=$1`
-	_, err := db.Exec(query, uid, delivery.Name, delivery.Phone, delivery.Zip, delivery.City, delivery.Address, delivery.Region, delivery.Email)
+func UpdatePayment(payment models.Payment, db *sqlx.DB) {
+	query := `UPDATE payments SET request_id=$2, currency=$3, provider=$4, amount=$5, payment_dt=$6, 
+	bank=$7, delivery_cost=$8, goods_total=$9, custom_fee=$10 WHERE transaction=$1`
+	_, err := db.Exec(query, payment.Transaction, payment.Request_id, payment.Currency, payment.Provider, payment.Amount, payment.Payment_dt,
+		payment.Bank, payment.Delivery_cost, payment.Goods_total, payment.Custom_fee)
 	if err != nil {
-		logrus.Info("error UpdateDelivery postgres")
+		logrus.Info("error UpdatePayment postgres")
+	}
+}
+
+func UpdateItems(items []models.Item, db *sqlx.DB) {
+	query := `UPDATE items SET request_id=$1, currency=$3, provider=$4, amount=$5, payment_dt=$6, 
+	bank=$7, delivery_cost=$8, goods_total=$9, custom_fee=$10, custom_fee=$11 WHERE track_number=$2`
+	for _, item := range items {
+		_, err := db.Exec(query, item.Chrt_id, item.Track_number, item.Price, item.Rid, item.Name, item.Sale,
+			item.Size, item.Total_price, item.Nm_id, item.Brand, item.Status)
+		if err != nil {
+			logrus.Info("error UpdateItems postgres")
+		}
 	}
 }
